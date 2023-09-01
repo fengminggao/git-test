d6bfc44739604157fed7cc199ef982d0cb6564f7
gaoming <2052712743@qq.com>
2023-09-01 15:09:16 +0800

    hot-fix four commit1

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git2");
         System.out.println("git3");
+        System.out.println("git4");
 
     }
 }
